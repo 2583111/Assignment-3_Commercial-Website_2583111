1406f84da0ccb383a2ad9c936fd48462d8e2065c
2583111 <soulsociety2004@gmail.com>
2024-10-04 02:34:53 +0200

    globe test

---
+++ JavaScript/footer.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const footer = document.querySelector("footer");
+
+    footer.innerHTML = `
+        <p>&copy; 2024 Haunted Webpage. All Rights Reserved.</p>
+        <p>Follow us on <a href="https://twitter.com/haunted" target="_blank">Twitter</a> 
+        <a href="https://facebook.com/haunted" target="_blank">Facebook</a></p>
+    `;
+});
---
+++ JavaScript/menu.js
@@ -0,0 +1,24 @@
+const root = "/WSOA3028A_2583111";
+
+const menuItems = [
+    { name: "Home", href: root + "/index.html" },
+    { name: "Theory", href: `${root}/Theory/index.html` },
+    { name: "Design", href: `${root}/Design/index.html` },
+];
+
+export function initialise(currentPage) {
+    const nav = document.querySelector("header > nav");
+    const ul = document.createElement("ul");
+    for (let menuItem of menuItems) {
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+        a.innerText = menuItem.name;
+        a.setAttribute("href", menuItem.href);
+        if (currentPage === menuItem.name) {
+            a.classList.add("current");
+        }
+        li.appendChild(a);
+        ul.appendChild(li);
+    }
+    nav.appendChild(ul);
+}
