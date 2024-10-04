f7112948d9c99174ba918de6c8a4cd479f4416ce
2583111 <soulsociety2004@gmail.com>
2024-10-04 03:20:09 +0200

    menu test

--- JavaScript/menu.js
+++ JavaScript/menu.js
@@ -1,22 +1,19 @@
 const root = "/Assignment-3_Commercial-Website_2583111";
 
 const menuItems = [
-    { name: "Home", href: "/index.html" },
-    { name: "Theory", href: "/Theory/index.html" },
-    { name: "Design", href: "/Design/index.html" },
+    { name: "Home", href: root + "/index.html" },
+    { name: "Theory", href: `${root}/Theory/index.html` },
+    { name: "Design", href: `${root}/Design/index.html` },
 ];
 
 export function initialise(currentPage) {
     const nav = document.querySelector("header > nav");
     const ul = document.createElement("ul");
-
     for (let menuItem of menuItems) {
         const li = document.createElement("li");
         const a = document.createElement("a");
         a.innerText = menuItem.name;
         a.setAttribute("href", menuItem.href);
-
-        // Highlight the current page by checking the name
         if (currentPage === menuItem.name) {
             a.classList.add("current");
         }
